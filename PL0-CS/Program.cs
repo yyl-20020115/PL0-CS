69533d0f3e1ff1b6faae575ae1cb7314f71f204b
YILIN <YILIN@DESKTOP-YILIN>
2022-06-27 00:17:06 +0800

    examples added

--- PL0-CS/Program.cs
+++ PL0-CS/Program.cs
@@ -1,19 +1,21 @@
 ﻿
 using PL0;
 
-if (args.Length < 2)
+if (args.Length < 1)
 {
-    Console.WriteLine("usage: PL0 Input Outputs");
+    Console.WriteLine("usage: PL0 <Input> [Output]");
     return 1;
 }
 
 
 using var reader = new StreamReader(args[0]);
-using var writer = new StreamWriter(args[1]);
+using var writer = args.Length >=2 
+    ? new StreamWriter(args[1]) 
+    : Console.Out
+    ;
 
-var parser = new Parser(reader,writer);
+var parser = new Parser(reader, writer);
 var program = parser.Parse();
-
 var evaluator = new Evaluator(reader, writer);
 program.Accept(evaluator);
 
